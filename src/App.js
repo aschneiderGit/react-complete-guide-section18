f4420c8d2e83faafe047a2e310decf2efed12ddc
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-26 16:27:34 +0100

    use AutSlice

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import {Fragment} from 'react';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import {useSelector} from 'react-redux';
 
 function App() {
+	const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
 	return (
 		<Fragment>
 			<Header />
-			<Auth />
-			<Counter />
+			{!isAuth && <Auth />}
+			{isAuth && <Counter />}
 		</Fragment>
 	);
 }
