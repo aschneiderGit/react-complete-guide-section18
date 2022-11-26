cd70efdbafe283ca95a0a69b5a9330287d0fda77
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-26 14:55:57 +0100

    increment and decrment with redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import {legacy_createStore as createStore} from 'redux';
 
 const counterReducer = (state = {counter: 0}, action) => {
-	if (action.type === 'increment') {
+	if (action.type === 'INCREMENT') {
 		return {
 			counter: state.counter + 1,
 		};
-	} else if (action.type === 'decrement') {
+	} else if (action.type === 'DECREMENT') {
 		return {
 			counter: state.counter - 1,
 		};
