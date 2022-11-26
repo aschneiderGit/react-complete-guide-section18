2302af0ad6845ca8b4862b92dd7cbaaa957f63d2
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-26 14:52:29 +0100

    get a data from store

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,15 @@ import {legacy_createStore as createStore} from 'redux';
 const counterReducer = (state = {counter: 0}, action) => {
 	if (action.type === 'increment') {
 		return {
-			countr: state.counter + 1,
+			counter: state.counter + 1,
 		};
 	} else if (action.type === 'decrement') {
 		return {
-			countr: state.counter - 1,
+			counter: state.counter - 1,
 		};
 	}
+
+	return state;
 };
 
 const store = createStore(counterReducer);
